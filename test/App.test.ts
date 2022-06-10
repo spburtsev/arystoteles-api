eef8102028f06ab077d967261ce0b46f8e2280bc
Sergei Burtsev <spburtsev@gmail.com>
2022-06-10 17:31:15 +0300

    Jest configuration added

--- test/App.test.ts
+++ test/App.test.ts
@@ -1,3 +1,5 @@
+import { Request, Response, NextFunction } from 'express';
+import ActivityController from '../src/controller/ActivityController';
 import request from 'supertest';
 import App from '../src/App';
 
@@ -7,4 +9,31 @@ it('should successfully create an App and fetch activities', function(done) {
     .get('/api/v1/activities')
     .expect(200)
     .end(done);
+});
+
+describe('Get all activities', () => {
+  const app = App.create();
+  let mockRequest: Partial<Request>;
+  let mockResponse: Partial<Response>;
+  let mockNext: NextFunction = () => {};
+  let responseData: any;
+
+  beforeEach(() => {
+    mockRequest = {};
+    mockResponse = {
+      json: jest.fn().mockImplementation(data => {
+        responseData = data;
+      }),
+    };
+  });
+
+  test('200 - activities', () => {
+    ActivityController.getAllActivities(
+      mockRequest as Request,
+      mockResponse as Response,
+      mockNext,
+    );
+    expect(responseData.status).toBe(200);
+    expect(responseData.data).toBeDefined();
+  });
 });
