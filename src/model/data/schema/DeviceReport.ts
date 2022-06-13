a71372b9eeb0bf5b2d6d83cc51b6d38684fab36a
Sergei Burtsev <spburtsev@gmail.com>
2022-06-13 16:16:15 +0300

    DeviceReport: Localized method added

--- src/model/data/schema/DeviceReport.ts
+++ src/model/data/schema/DeviceReport.ts
@@ -2,12 +2,15 @@ import { Model, Schema, Document, model } from 'mongoose';
 import { IDevice } from './Device';
 import _LocalizedString from './types/_LocalizedString';
 import LocalizedString from '../LocalizedString';
+import AppLocale from '../../enum/AppLocale';
 
 export interface IDeviceReport extends Document {
   createdAt: Date;
   device: IDevice;
   message: LocalizedString;
   data: string;
+
+  localized: (locale: AppLocale) => any;
 }
 
 const DeviceReportSchema = new Schema({
@@ -16,6 +19,14 @@ const DeviceReportSchema = new Schema({
   message: { type: _LocalizedString, required: true },
   data: { type: String, required: false },
 });
+
+DeviceReportSchema.methods.localized = function(locale: AppLocale) {
+  return {
+    createdAt: this.createdAt,
+    message: this.message[locale],
+    data: this.data,
+  };
+};
 
 const DeviceReport: Model<IDeviceReport> = model(
   'DeviceReport',
